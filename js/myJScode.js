2b507f3c926ec06cbad39d73dfbc972a2e0cb9d9
HanderLet <110351302+HanderLet@users.noreply.github.com>
2022-12-08 19:52:29 +0400

    Недоработанная 11-12 дз

--- js/myJScode.js
+++ js/myJScode.js
@@ -2,6 +2,29 @@ $(document).ready(function(){
     $(".area").css("display", "none");
 });
 
+
+$(document).ready(function(){
+            let options = {
+            threshold: [0.5]
+        };
+        let observer = new IntersectionObserver(onEntry, options);
+        let elements = $('.element-animation');
+
+        elements.each((i, el) => {
+            observer.observe(el);
+        });
+
+
+        function onEntry(entry) {
+            entry.forEach(change => {
+                if (change.isIntersecting) {
+                    change.target.classList.add('element-show');
+                }
+            });
+        }
+});
+
+
 $('a[href^="#"]').click(function(){
    let varHref = $(this).attr("href");
     $('html,body').animation({scrollTop: $(valHref).offset().top - 50 + 'px'});
@@ -36,3 +59,39 @@ $('.image-link').magnificPopup({
     type: 'image'
 });
 
+    
+let optionsStat = {
+        threshold: [0.5]
+    };
+    let observerStat = new IntersectionObserver(onEntryStat, optionsStat);
+    let elementsStat = $('.runNumbers');
+
+    elementsStat.each((i, el) => {
+        observerStat.observe(el);
+    });
+
+function onEntryStat(entry) {
+    entry.forEach(change => {
+        if (change.isIntersecting) {
+            $(".runNumbers").each(function(){
+            $(this).prop('Counter',0).animate({
+            Counter:$(this).text()
+        },{
+            duration:4000,
+            easing:'swing',
+            step:function(now){
+            $(this).text(Math.ceil(now));
+       }
+            });
+        });
+      }
+   });
+}
+
+setTimeout(function () {
+    const modalWindow = new bootstrap.Modal('#exampleModalToggle', {
+        keyboard: false
+    });
+        modalToggle = document.getElementById('exampleModalToggle'); 
+        modalWindow.show(modalToggle);
+    }, 10000);
